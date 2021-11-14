d39249bab8131ba832cfabfc820332ca3cce51cc
Zoroter <zorotes@gmail.com>
2021-11-14 16:34:09 +0100

    trying mailer config for deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,7 +44,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   config.action_cable.url = 'wss://doggo-shouts.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = [ 'https://doggo-shouts.herokuapp.com', /http:\/\/doggo-shouts.herokuapp.com.*/ ]
+  config.action_cable.allowed_request_origins = [ 'https://doggo-shouts.herokuapp.com', /https:\/\/doggo-shouts.herokuapp.com.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
@@ -90,5 +90,18 @@ Rails.application.configure do
   end
 
   # Do not dump schema after migrations.
+  config.action_mailer.default_url_options = { :host => 'https://doggo-shouts.herokuapp.com' }
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    domain: "gmail.com",
+    port: 587,
+    user_name: "DoggoShouts@gmail.com",
+    password: Rails.application.credentials.dig(:mail_password),
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 end
--- config/recaptcha.rb
+++
@@ -1,5 +0,0 @@
-Recaptcha.configure do |config|
-    config.site_key  = Rails.application.credentials.dig(:google, :captcha_public)
-    config.secret_key = Rails.application.credentials.dig(:google, :captcha_private)
-end
-
