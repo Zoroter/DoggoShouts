b6ca13a1efdcacbf13c74c91d4445e2d68fe1eb8
Zoroter <zorotes@gmail.com>
2021-11-14 12:05:11 +0100

    recaptcha initailizer changed tests

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,13 @@
-Recaptcha.configure do |config|
-    config.site_key  = Rails.application.credentials.google[:captcha_public]
-    config.secret_key = Rails.application.credentials.google[:captcha_private]
+if Rails.env.development? || Rails.env.test?
+    Recaptcha.configure do |config|
+        config.site_key  = Rails.application.credentials.google[:captcha_public]
+        config.secret_key = Rails.application.credentials.google[:captcha_private]
+    end
+end
+
+if Rails.env.production?
+    Recaptcha.configure do |config|
+        config.public_key  = Rails.application.credentials.google[:captcha_public]
+        config.private_key = Rails.application.credentials.google[:captcha_private]
+    end
 end
