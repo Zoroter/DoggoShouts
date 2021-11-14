072dfb3a0ddca5b236db99cce0ff309696cdc718
Zoroter <zorotes@gmail.com>
2021-11-14 12:17:51 +0100

    change in digging credentials in recaptcha

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'puma', '~> 4.3', '>= 4.3.9'
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
+gem 'recaptcha', :require => 'recaptcha/rails'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'mini_racer', platforms: :ruby
 gem 'devise'
@@ -21,7 +22,7 @@ gem 'bootstrap'
 gem 'jquery-rails'
 # Use CoffeeScript for .coffee assets and views
 gem 'redis'
-gem 'recaptcha', :require => 'recaptcha/rails'
+
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
 
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,13 +1,6 @@
-if Rails.env.development? || Rails.env.test?
-    Recaptcha.configure do |config|
-        config.site_key  = Rails.application.credentials.google[:captcha_public]
-        config.secret_key = Rails.application.credentials.google[:captcha_private]
-    end
+Recaptcha.configure do |config|
+    config.site_key  = Rails.application.credentials.dig(:google, :captcha_public)
+    config.secret_key = Rails.application.credentials.dig(:google, :captcha_private)
 end
 
-if Rails.env.production?
-    Recaptcha.configure do |config|
-        config.public_key  = Rails.application.credentials.google[:captcha_public]
-        config.private_key = Rails.application.credentials.google[:captcha_private]
-    end
-end
+
